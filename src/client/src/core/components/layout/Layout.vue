3b62c01f3f75c9412c615692f7bcbe72c397ed43
Asuna <157031105+xKaizen23@users.noreply.github.com>
2025-04-22 13:54:11 +0800

    Revert to original layout.vue

--- src/client/src/core/components/layout/Layout.vue
+++ src/client/src/core/components/layout/Layout.vue
@@ -1,10 +1,7 @@
 <template>
-  <oxd-layout
-    :class="{
-      'orangehrm-upgrade-layout': showUpgrade,
-    }"
-    v-bind="$attrs"
-  >
+  <oxd-layout :class="{
+    'orangehrm-upgrade-layout': showUpgrade,
+  }" v-bind="$attrs">
     <template v-for="(_, name) in $slots" #[name]="slotData">
       <slot :name="name" v-bind="slotData" />
     </template>
@@ -13,12 +10,7 @@
     </template>
     <template #user-actions>
       <li>
-        <a
-          href="#"
-          role="menuitem"
-          class="oxd-userdropdown-link"
-          @click="openAboutModel"
-        >
+        <a href="#" role="menuitem" class="oxd-userdropdown-link" @click="openAboutModel">
           {{ $t('general.about') }}
         </a>
       </li>
@@ -28,11 +20,7 @@
         </a>
       </li>
       <li v-if="updatePasswordUrl">
-        <a
-          :href="updatePasswordUrl"
-          role="menuitem"
-          class="oxd-userdropdown-link"
-        >
+        <a :href="updatePasswordUrl" role="menuitem" class="oxd-userdropdown-link">
           {{ $t('general.change_password') }}
         </a>
       </li>
@@ -43,21 +31,17 @@
       </li>
     </template>
     <template #nav-actions>
-      <oxd-icon-button
-        name="question-lg"
-        :title="$t('general.help')"
-        @click="onClickSupport"
-      />
+      <oxd-icon-button name="question-lg" :title="$t('general.help')" @click="onClickSupport" />
     </template>
   </oxd-layout>
   <about v-if="showAboutModel" @close="closeAboutModel"></about>
 </template>
 
 <script>
-import {provide, readonly, ref} from 'vue';
+import { provide, readonly, ref } from 'vue';
 import About from '@/core/pages/About.vue';
-import {OxdLayout} from '@ohrm/oxd';
-import {dateFormatKey} from '@/core/util/composable/useDateFormat';
+import { OxdLayout } from '@ohrm/oxd';
+import { dateFormatKey } from '@/core/util/composable/useDateFormat';
 // import UpgradeButton from '@/core/components/buttons/UpgradeButton.vue';
 
 export default {
@@ -98,59 +82,42 @@ export default {
     },
   },
   setup(props) {
-  const showAboutModel = ref(false);
-  provide('permissions', readonly(props.permissions));
-  provide(dateFormatKey, readonly(props.dateFormat));
+    const showAboutModel = ref(false);
+    provide('permissions', readonly(props.permissions));
+    provide(dateFormatKey, readonly(props.dateFormat));
 
-  const openAboutModel = () => {
-    showAboutModel.value = true;
-  };
+    const openAboutModel = () => {
+      showAboutModel.value = true;
+    };
 
-  const closeAboutModel = () => {
-    showAboutModel.value = false;
-  };
+    const closeAboutModel = () => {
+      showAboutModel.value = false;
+    };
 
-  const onClickSupport = () => {
-    if (props.helpUrl) window.open(props.helpUrl, '_blank');
-  };
+    const onClickSupport = () => {
+      if (props.helpUrl) window.open(props.helpUrl, '_blank');
+    };
 
-  // ✅ Inject Admin Attendance menu item
-  onMounted(() => {
-    const interval = setInterval(() => {
-      const menu = document.querySelector('.oxd-dropdown-menu');
-      if (menu && !document.querySelector('#admin-attendance-link')) {
-        const li = document.createElement('li');
-        li.innerHTML = `
-          <a id="admin-attendance-link" class="oxd-topbar-body-nav-tab-link" href="#">
-            Admin Attendance
-          </a>
-        `;
-        li.onclick = () => {
-          window.location.href = '/web/index.php/time/adminAttendance';
-        };
-        menu.appendChild(li);
-        clearInterval(interval);
-      }
-    }, 300); // Retry until dropdown is rendered
-  });
-
-  return {
-    onClickSupport,
-    showAboutModel,
-    openAboutModel,
-    closeAboutModel,
-  };
-}
-
+    return {
+      onClickSupport,
+      showAboutModel,
+      openAboutModel,
+      closeAboutModel,
+    };
+  },
+};
 </script>
 
 <style lang="scss">
 .orangehrm-upgrade-layout {
   .oxd-topbar-header-userarea {
     display: flex;
-    justify-content: flex-end; /* Moves it to the right */
-    align-items: center; /* Ensures it stays vertically aligned */
-    margin-left: auto; /* Pushes it to the right */
+    justify-content: flex-end;
+    /* Moves it to the right */
+    align-items: center;
+    /* Ensures it stays vertically aligned */
+    margin-left: auto;
+    /* Pushes it to the right */
   }
 }
 </style>
